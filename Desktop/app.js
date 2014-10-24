913386d7a9f1a9ba936fffab8a4e0efebb76caa2
Vin424 <Vin@Irvings-MacBook-Pro-3.local>
2014-10-24 03:07:00 -0400

    no message

--- Desktop/app.js
+++ Desktop/app.js
@@ -38,7 +38,7 @@ if ('development' == app.get('env')) {
 }
 
 app.all('/', function(req, res, next) {
-   es.header('Access-Control-Allow-Origin', "*")
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
